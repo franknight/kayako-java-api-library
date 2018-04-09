2ddbc4e441d32d3ed828889ef2378dcc1a3ad3a7
rfajardo2008 <rodolfo.fajardo@icloud.com>
2018-04-09 17:11:25 -0500

    class files eliminated and BaseGroup.java added to commit and Push

---
+++ src/com/kayako/api/user/BaseGroup.java
@@ -0,0 +1,81 @@
+package com.kayako.api.user;
+
+import com.kayako.api.rest.KEntity;
+
+public abstract class BaseGroup extends KEntity {
+    /**
+     * Base Group identifier.
+     *
+     * @apiField
+     * @var int
+     */
+    protected int id;
+
+    /**
+     * Base Group title.
+     *
+     * @apiField
+     * @var string
+     */
+    protected String title;
+    
+    /**
+     * Gets title.
+     *
+     * @return the title
+     */
+    public String getTitle() {
+
+        return title;
+    }
+
+    /**
+     * Sets title.
+     *
+     * @param title the title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Gets title.
+     *
+     * @return the title
+     */
+    public int getId() {
+
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets read only?
+     *
+     * @return readOnly
+     */
+    public Boolean getReadOnly() {
+
+        return readOnly;
+    }
+
+    /**
+     * Sets read only.
+     *
+     * @param readOnly
+     */
+    public BaseGroup setReadOnly(Boolean readOnly) {
+        this.readOnly = readOnly;
+        return this;
+    }
+
+
+}
