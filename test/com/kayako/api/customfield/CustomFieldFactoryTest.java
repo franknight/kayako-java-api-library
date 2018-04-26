daa2c9b3e0b0fe77a3f0c8976cf3996f240b8f5c
lfcamara <lfcamara.ti@gmail.com>
2018-04-26 20:38:12 -0300

    Adjust the test file

--- test/com/kayako/api/customfield/CustomFieldFactoryTest.java
+++ test/com/kayako/api/customfield/CustomFieldFactoryTest.java
@@ -1,154 +1,178 @@
 package com.kayako.api.customfield;
 
-import static org.easymock.EasyMock.mock;
 import static org.easymock.EasyMock.expect;
-import static org.hamcrest.CoreMatchers.is;
+import static org.easymock.EasyMock.mock;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.powermock.api.easymock.PowerMock.createMockAndExpectNew;
 import static org.powermock.api.easymock.PowerMock.replayAll;
 import static org.powermock.api.easymock.PowerMock.verifyAll;
-import static org.powermock.api.easymock.PowerMock.createMockAndExpectNew;
 
-import org.junit.rules.ExpectedException;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.Before;
-import org.junit.runner.RunWith;
 import org.junit.rules.ErrorCollector;
-import com.kayako.api.rest.RawArrayElement;
-import com.kayako.api.exception.KayakoException;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
 import com.kayako.api.configuration.Configuration;
 import com.kayako.api.enums.CustomFieldDefinitionTypeEnum;
+import com.kayako.api.exception.KayakoException;
+import com.kayako.api.rest.RawArrayElement;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(CustomFieldFactory.class)
 public class CustomFieldFactoryTest {
 
-    private static final String TYPE = "type";
-    private static final String API_KEY = "apiKey";
-    private static final String BASE_URL = "baseUrl";
-    private static final String SECRET_KEY = "secretKey";
-    private static final String NAME_KEY = "name";
-    private static final String TITLE_KEY = "title";
-    private static final String FILE_NAME = "filename";
-    private static final String STR_VALUE = "strValue";
-    private static final String STR_VALUE4NAME = "strValue4Name";
-    private static final String STR_VALUE4TITLE= "strValue4Title";
-    private static final String INVALID_ELEMENT_NAME = "invalidElementName";
-    private static final String INVALID_XML_ELEMENT_MSG = "Invalid XML Element";
+	private static final String TYPE = "type";
+	private static final String API_KEY = "apiKey";
+	private static final String BASE_URL = "baseUrl";
+	private static final String SECRET_KEY = "secretKey";
+	private static final String NAME_KEY = "name";
+	private static final String TITLE_KEY = "title";
+	private static final String FILE_NAME = "filename";
+	private static final String STR_VALUE = "strValue";
+	private static final String STR_VALUE4NAME = "strValue4Name";
+	private static final String STR_VALUE4TITLE = "strValue4Title";
+	private static final String INVALID_ELEMENT_NAME = "invalidElementName";
+	private static final String INVALID_XML_ELEMENT_MSG = "Invalid XML Element";
 
-    private RawArrayElement rawArrayElement;
-    private CustomFieldGroup mockedCustomFieldGroup;
+	private RawArrayElement rawArrayElement;
+	private CustomFieldGroup customFieldGroupMocked;
 
-    @Rule
-    public final ExpectedException thrown = ExpectedException.none();
+	@Rule
+	public final ExpectedException thrown = ExpectedException.none();
 
-    @Rule
-    public final ErrorCollector collector = new ErrorCollector();
+	@Rule
+	public final ErrorCollector collector = new ErrorCollector();
 
-    @Before
-    public void setUp(){
-        mockedCustomFieldGroup = mock(CustomFieldGroup.class);
+	@Before
+	public void setUp() {
+		customFieldGroupMocked = mock(CustomFieldGroup.class);
 
-        rawArrayElement = new RawArrayElement(CustomField.getObjectXmlName());
-        rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.TEXT.toString());
+		rawArrayElement = new RawArrayElement(CustomField.getObjectXmlName());
+		rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.TEXT.toString());
 
-        Configuration.init(BASE_URL, API_KEY, SECRET_KEY);
-    }
+		Configuration.init(BASE_URL, API_KEY, SECRET_KEY);
+	}
 
-    @Test
-    public void shouldCreateCustomFieldFile() throws KayakoException {
-        rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.FILE.getString());
-        rawArrayElement.setAttribute(FILE_NAME, STR_VALUE);
-        CustomField customField = CustomFieldFactory.createCustomField(mockedCustomFieldGroup, rawArrayElement);
+	@Test
+	public void shouldCreateCustomFieldFile() throws KayakoException {
+		// Arrange
+		rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.FILE.getString());
+		rawArrayElement.setAttribute(FILE_NAME, STR_VALUE);
 
-        collector.checkThat(customField, instanceOf(CustomFieldFile.class));
-        collector.checkThat(((CustomFieldFile)customField).getFileName(), equalTo(STR_VALUE));
-    }
+		// Act
+		CustomField customField = CustomFieldFactory.createCustomField(customFieldGroupMocked, rawArrayElement);
 
-    @Test
-    public void shouldCreateCustomFieldLinkedSelect() throws Exception {
-        rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.LINKED_SELECT.getString());
+		// Assert
+		collector.checkThat(customField, instanceOf(CustomFieldFile.class));
+		collector.checkThat(((CustomFieldFile) customField).getFileName(), equalTo(STR_VALUE));
+	}
 
-        CustomFieldLinkedSelect mockedCustomFieldLinkedSelect = createMockAndExpectNew(CustomFieldLinkedSelect.class, mockedCustomFieldGroup);
-        expect(mockedCustomFieldLinkedSelect.populate(rawArrayElement)).andReturn(mockedCustomFieldLinkedSelect);
+	@Test
+	@PrepareForTest(CustomFieldFactory.class)
+	public void shouldCreateCustomFieldLinkedSelect() throws Exception {
+		// Arrange
+		rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.LINKED_SELECT.getString());
+		CustomFieldLinkedSelect mockedCustomFieldLinkedSelect = mock(CustomFieldLinkedSelect.class);
+		expect(mockedCustomFieldLinkedSelect.populate(rawArrayElement)).andReturn(mockedCustomFieldLinkedSelect);
+		
+		// Act
+		replayAll();
+		CustomField customField = CustomFieldFactory.createCustomField(customFieldGroupMocked, rawArrayElement);
+		verifyAll();
+		
+		// Assert
+		collector.checkThat(customField, instanceOf(CustomFieldLinkedSelect.class));
+		collector.checkThat(customField, is(notNullValue()));
+	}
 
-        replayAll();
-        CustomField customField = CustomFieldFactory.createCustomField(mockedCustomFieldGroup, rawArrayElement);
-        verifyAll();
-        collector.checkThat(customField, instanceOf(CustomFieldLinkedSelect.class));
-        collector.checkThat(customField, is(notNullValue()));
-    }
+	@Test
+	@PrepareForTest(CustomFieldFactory.class)
+	public void shouldCreateCustomFieldMultiSelect() throws Exception {
+		// Arrange
+		rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.MULTI_SELECT.getString());
+		CustomFieldMultiSelect mockedCustomFieldMultiSelect = createMockAndExpectNew(CustomFieldMultiSelect.class, customFieldGroupMocked);
+		expect(mockedCustomFieldMultiSelect.populate(rawArrayElement)).andReturn(mockedCustomFieldMultiSelect);
 
-    @Test
-    public void shouldCreateCustomFieldMultiSelect() throws Exception {
-        rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.MULTI_SELECT.getString());
+		// Act
+		replayAll();
+		CustomField customField = CustomFieldFactory.createCustomField(customFieldGroupMocked, rawArrayElement);
+		verifyAll();
 
-        CustomFieldMultiSelect mockedCustomFieldMultiSelect = createMockAndExpectNew(CustomFieldMultiSelect.class, mockedCustomFieldGroup);
-        expect(mockedCustomFieldMultiSelect.populate(rawArrayElement)).andReturn(mockedCustomFieldMultiSelect);
+		// Assert
+		collector.checkThat(customField, instanceOf(CustomFieldMultiSelect.class));
+		collector.checkThat(customField, is(notNullValue()));
+	}
 
-        replayAll();
-        CustomField customField = CustomFieldFactory.createCustomField(mockedCustomFieldGroup, rawArrayElement);
-        verifyAll();
-        collector.checkThat(customField, instanceOf(CustomFieldMultiSelect.class));
-        collector.checkThat(customField, is(notNullValue()));
-    }
+	@Test
+	@PrepareForTest(CustomFieldFactory.class)
+	public void shouldCreateCustomFieldSelect() throws Exception {
+		// Arrange
+		rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.SELECT.getString());
+		CustomFieldSelect mockedCustomFieldSelect = createMockAndExpectNew(CustomFieldSelect.class, customFieldGroupMocked);
+		expect(mockedCustomFieldSelect.populate(rawArrayElement)).andReturn(mockedCustomFieldSelect);
 
-    @Test
-    public void shouldCreateCustomFieldSelect() throws Exception {
-        rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.SELECT.getString());
+		// Act
+		replayAll();
+		CustomField customField = CustomFieldFactory.createCustomField(customFieldGroupMocked, rawArrayElement);
+		verifyAll();
 
-        CustomFieldSelect mockedCustomFieldSelect = createMockAndExpectNew(CustomFieldSelect.class, mockedCustomFieldGroup);
-        expect(mockedCustomFieldSelect.populate(rawArrayElement)).andReturn(mockedCustomFieldSelect);
+		// Assert
+		collector.checkThat(customField, instanceOf(CustomFieldSelect.class));
+		collector.checkThat(customField, is(notNullValue()));
+	}
 
-        replayAll();
-        CustomField customField = CustomFieldFactory.createCustomField(mockedCustomFieldGroup, rawArrayElement);
-        verifyAll();
-        collector.checkThat(customField, instanceOf(CustomFieldSelect.class));
-        collector.checkThat(customField, is(notNullValue()));
-    }
+	@Test
+	public void shouldCreateCustomFieldDate() throws KayakoException {
+		// Arrange
+		rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.DATE.getString());
+		rawArrayElement.setElementName(CustomFieldDate.getObjectXmlName());
 
-    @Test
-    public void shouldCreateCustomFieldDate() throws KayakoException {
-        rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.DATE.getString());
-        rawArrayElement.setElementName(CustomFieldDate.getObjectXmlName());
-        CustomField customField = CustomFieldFactory.createCustomField(mockedCustomFieldGroup, rawArrayElement);
+		// Act
+		CustomField customField = CustomFieldFactory.createCustomField(customFieldGroupMocked, rawArrayElement);
 
-        collector.checkThat(customField, instanceOf(CustomFieldDate.class));
-        collector.checkThat(customField, is(notNullValue()));
-    }
+		// Assert
+		collector.checkThat(customField, instanceOf(CustomFieldDate.class));
+		collector.checkThat(customField, is(notNullValue()));
+	}
 
-    @Test
-    public void shouldCreateCustomField() throws KayakoException {
-        rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.TEXT.getString());
-        rawArrayElement.setAttribute(TITLE_KEY, STR_VALUE4TITLE);
-        rawArrayElement.setAttribute(NAME_KEY, STR_VALUE4NAME);
-        rawArrayElement.setElementName(CustomField.getObjectXmlName());
-        CustomField customField = CustomFieldFactory.createCustomField(mockedCustomFieldGroup, rawArrayElement);
+	@Test
+	public void shouldCreateCustomField() throws KayakoException {
+		// Arrange
+		rawArrayElement.setAttribute(TYPE, CustomFieldDefinitionTypeEnum.TEXT.getString());
+		rawArrayElement.setAttribute(TITLE_KEY, STR_VALUE4TITLE);
+		rawArrayElement.setAttribute(NAME_KEY, STR_VALUE4NAME);
+		rawArrayElement.setElementName(CustomField.getObjectXmlName());
 
-        collector.checkThat(customField, is(notNullValue()));
-        collector.checkThat(customField.getName(), equalTo(STR_VALUE4NAME));
-        collector.checkThat(customField.getTitle(), equalTo(STR_VALUE4TITLE));
-    }
+		// Act
+		CustomField customField = CustomFieldFactory.createCustomField(customFieldGroupMocked, rawArrayElement);
 
-    @Test
-    public void givenNullElementWhenCreateCustomFieldThenKayakoException() throws KayakoException {
-        thrown.expect(KayakoException.class);
-        thrown.expectMessage(containsString(INVALID_XML_ELEMENT_MSG));
+		// Assert
+		collector.checkThat(customField, is(notNullValue()));
+		collector.checkThat(customField.getName(), equalTo(STR_VALUE4NAME));
+		collector.checkThat(customField.getTitle(), equalTo(STR_VALUE4TITLE));
+	}
 
-        CustomFieldFactory.createCustomField(mockedCustomFieldGroup, null);
-    }
+	@Test
+	public void givenNullElementWhenCreateCustomFieldThenKayakoException() throws KayakoException {
+		thrown.expect(KayakoException.class);
+		thrown.expectMessage(containsString(INVALID_XML_ELEMENT_MSG));
 
-    @Test
-    public void givenElementWithInvalidElementNameWhenCreateCustomFieldThenKayakoException() throws KayakoException {
-        thrown.expect(KayakoException.class);
-        thrown.expectMessage(containsString(INVALID_XML_ELEMENT_MSG));
+		CustomFieldFactory.createCustomField(customFieldGroupMocked, null);
+	}
 
-        rawArrayElement.setElementName(INVALID_ELEMENT_NAME);
-        CustomFieldFactory.createCustomField(mockedCustomFieldGroup, rawArrayElement);
-    }
+	@Test
+	public void givenElementWithInvalidElementNameWhenCreateCustomFieldThenKayakoException() throws KayakoException {
+		thrown.expect(KayakoException.class);
+		thrown.expectMessage(containsString(INVALID_XML_ELEMENT_MSG));
+
+		rawArrayElement.setElementName(INVALID_ELEMENT_NAME);
+		CustomFieldFactory.createCustomField(customFieldGroupMocked, rawArrayElement);
+	}
 }
